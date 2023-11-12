11611482d8b8e0ae18dc54c53b560db326273c02
thargard <boris.margolin.1301@gmail.com>
2023-11-12 17:18:04 +0300

    +Main2

---
+++ src/Main2.java
@@ -0,0 +1,10 @@
+import com.example.SecondPart;
+
+public class Main2 {
+    public static void main(String[] args){
+        SecondPart sp = new SecondPart("Салон красоты");
+        sp.setVisible(true);
+        sp.setResizable(false);
+        sp.setLocationRelativeTo(null);
+    }
+}
